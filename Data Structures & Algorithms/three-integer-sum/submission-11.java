b17af2dff2dd9de79428f73839068623429f1276
Raktim Kalita <rktmkalita2@gmail.com>
2026-08-30 22:21:24 +0530

    Add: three-integer-sum - submission-11

---
+++ Data Structures & Algorithms/three-integer-sum/submission-11.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(nums[i],i);
+            for(int j=i+1;j<nums.length;j++){
+                int sum = nums[i]+nums[j];
+                if(map.containsKey(-sum)){
+                    if(map.get(-sum)!=i && map.get(-sum)!=j){
+                        List<Integer> list = new ArrayList<>(List.of(nums[i],nums[j],-sum));
+                        Collections.sort(list);
+                        set.add(list);
+                    }
+                }
+                map.put(nums[j],j);
+            }
+        }
+        return List.copyOf(set);
+    }
+}
