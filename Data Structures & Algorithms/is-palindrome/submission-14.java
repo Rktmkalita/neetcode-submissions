e36ad0d8b54915feaa956b17fd00c45cb8723572
Raktim Kalita <rktmkalita2@gmail.com>
2026-08-30 21:41:14 +0530

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        int l=0, r=s.length()-1;
+        while(l<r){
+            while(l<r && !Character.isLetterOrDigit(s.charAt(l))){
+                l++;
+                System.out.println(s.charAt(l));
+            }
+            while(l<r && !Character.isLetterOrDigit(s.charAt(r))){
+                r--;
+                System.out.println(s.charAt(l) + " " + s.charAt(r));
+            }
+            if(s.charAt(l)!=s.charAt(r)){
+                System.out.println("comp "+s.charAt(l) + " " + s.charAt(r));
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
