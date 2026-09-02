749f1c55b7b902bcd3c137b38eb092fff72b2f9a
Raktim Kalita <rktmkalita2@gmail.com>
2026-09-02 21:29:38 +0530

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l=0,r=0;
+        int maxFrq = 0;
+        int res = 0;
+        int[] charMap = new int[26];
+        while(r<s.length()){
+            char ch = s.charAt(r);
+            int len = r-l+1;
+            charMap[ch-'A']++;
+            maxFrq = findMax(charMap);
+            if(len - maxFrq <= k){
+                // System.out.println("if: "+ch);
+                res = Math.max(res,len);
+            }else{
+                // System.out.println("else: "+s.charAt(l));
+                charMap[s.charAt(l)-'A']--;
+                l++;
+            }
+            r++;
+        }
+        return res;
+    }
+
+    private int findMax(int[] arr){
+        int max = 0;
+        for(int n : arr){
+            // System.out.print(n+" ");
+            if(n>max) max=n;
+        }
+        // System.out.println(max);
+        return max;
+    }
+}
