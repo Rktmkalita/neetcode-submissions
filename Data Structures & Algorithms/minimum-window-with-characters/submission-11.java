85d61c1548a5c60d68acd2319f15ea143430a92f
Raktim Kalita <rktmkalita2@gmail.com>
2026-09-04 22:20:34 +0530

    Add: minimum-window-with-characters - submission-11

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-11.java
@@ -0,0 +1,39 @@
+class Solution {
+    int[] tArr = new int[256];
+    int[] sArr = new int[256];
+    public String minWindow(String s, String t) {
+        if(s.length()<t.length()) return "";
+        String res="";
+        int l=0;
+        int r;
+        int minLen=Integer.MAX_VALUE;
+        for(char ch : t.toCharArray()){
+            tArr[ch]++;
+        }
+        for(r=0;r<s.length();r++){
+            char c = s.charAt(r);
+            if(tArr[c]!=0){
+                sArr[c]++;
+            }
+            while(check()){
+                int len = r-l+1;
+                if(minLen>len){
+                    res = s.substring(l,r+1);
+                    minLen=len;
+                }
+                sArr[s.charAt(l)]--;
+                l++;
+            }
+        }
+        return res;
+    }
+
+    private boolean check(){
+        for(int i=0;i<256;i++){
+            if(tArr[i]!=0 && sArr[i]<tArr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
